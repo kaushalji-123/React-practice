45ce10b27e2b49d4a9654d6f89c48a7fd9a7e9c1
kaushalji-123 <divyanshu838283fb@gmail.com>
2023-09-03 17:55:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World App</h1>
+      <SignUp />
     </div>
   );
 }
